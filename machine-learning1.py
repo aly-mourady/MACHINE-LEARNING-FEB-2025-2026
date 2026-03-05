59e3010036d8b9aaf8679b19e75e4a314fe4a5ea
Alymourady <aly@mourady.net>
2026-03-06 01:04:22 +0200

    lab1 task

--- machine-learning1.py
+++ machine-learning1.py
@@ -7,10 +7,20 @@
 
 # TODO: Update the main function to your needs or remove it.
 
-
+import pandas as pd
+import numpy as np
 def main() -> None:
-    print("Start coding in Python today!")
-
+    file_name = "Public Datasets\\covid_19_clean_complete.csv.zip"
+    df = pd.read_csv(file_name, compression='zip')
+    #our objective is to predict the number of deaths in the coming days,Y(output), based on the number of confirmed cases,X(input)
+    print("The number of null values in the dataset is: ", df.isnull().sum())
+    #as we can see in the previous print, there are 34404 null values in the Province/State column.
+    #the first issue we face is the amount of null values in the dataset which may affect our estimate number of deaths
+    #for that reason, we will remove Province/State as it includes half the null values of the dataset
+    df.drop("Province/State", inplace=True, axis=1)
+    #the second issue is the date column, as it is a string (the wrong data type for dates)
+    df["Date"] = pd.to_datetime(df["Date"])
+    df.info()
 
 if __name__ == "__main__":
     main()
